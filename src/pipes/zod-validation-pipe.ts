c1e65992583b05417fe153a97d8a6ca1c3297eed
Asaf Naory <asaf.naory1@gmail.com>
2023-12-02 12:00:53 +0200

    Remove console.log statements and improve error handling in ZodValidationPipe

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -6,12 +6,10 @@ export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodObject<any>) {}
 
   transform(value: unknown) {
-    console.log('ZodValidationPipe');
     try {
       this.schema.strict().parse(value);
     } catch (err: unknown) {
       const error = err as ZodError;
-      console.log(fromZodError(error));
       throw new BadRequestException('Validation failed' + fromZodError(error));
     }
     return value;
