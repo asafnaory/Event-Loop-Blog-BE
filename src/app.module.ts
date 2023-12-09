bf44ef89cd251dfe09464cd8c93a88af662f5e97
Asaf Naory <asaf.naory1@gmail.com>
2023-12-09 18:14:33 +0200

    Add logging interceptor to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './blog/blog.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './logging-interceptor/logging-interceptor.interceptor';
 
 @Module({
   imports: [BlogModule, PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/logging-interceptor/logging-interceptor.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    console.log('Request Body:', request.body);
+
+    return next.handle().pipe(
+      tap((response) => {
+        console.log(`Response: ${JSON.stringify(response)}`);
+      }),
+    );
+  }
+}
